89e16f4a64f543e6e52831d4a944ee72e05d963b
KrisGru <k.gruszczynski1002@gmail.com>
2022-03-24 21:03:45 +0100

    added database on the cloud

--- prisma/migrations/20220324195808_create_db/migration.sql
+++ prisma/migrations/20220324195808_create_db/migration.sql
@@ -1,14 +1,15 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'DEVELOPER');
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT E'DEVELOPER',
+    "role" TEXT NOT NULL DEFAULT E'DEVELOPER',
     "email" TEXT NOT NULL,
     "avatar" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
